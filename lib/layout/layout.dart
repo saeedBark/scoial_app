ac80ff01140f4d3a9e0be54c50495b3fe00f7d41
saeedBark <109968682+saeedBark@users.noreply.github.com>
2023-03-09 05:05:01 +0000

    first push

---
+++ lib/layout/layout.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LayoutScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('saeed'),
+    );
+  }
+}
